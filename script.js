8aa614b217a1f6b4528d5abcf9c07a7a86deb654
Itamar Haimov <haimov135@gmail.com>
2021-10-21 15:13:27 +0300

    first JS Functions

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("js File connected")
+const title = document.querySelector("h1")
+const words = document.querySelector(".words")
+const colors = document.querySelector(".colors")
+
+words.addEventListener('click', function(){
+    title.textContent += " Blah"
+})
